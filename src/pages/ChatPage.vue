df3f1495a8c9afc31fd279ecfc2aa10fb4165343
Felix <jonfelixrico@gmail.com>
2023-02-05 00:13:20 +0800

    Implement sending

--- src/pages/ChatPage.vue
+++ src/pages/ChatPage.vue
@@ -1,9 +1,42 @@
 <template>
-  <div></div>
+  <div class="column">
+    <div class="col"></div>
+    <!-- TODO add validation; must not be blank -->
+    <q-form @submit="handleSubmit">
+      <div class="row">
+        <q-input type="textarea" v-model="content" />
+      </div>
+    </q-form>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { usePocketbase } from 'src/services/pocketbase.service'
+import { defineComponent, ref } from 'vue'
 
-export default defineComponent({})
+export default defineComponent({
+  setup() {
+    const pb = usePocketbase()
+    const content = ref('')
+
+    return {
+      content,
+      pb,
+    }
+  },
+
+  methods: {
+    async handleSubmit() {
+      this.sendMessage(this.content)
+    },
+
+    async sendMessage(content: string) {
+      await this.pb.collection('messages').create({
+        content,
+        senderId: [this.pb.authStore.model?.id],
+        chatRoomId: this.$route.params.chatId,
+      })
+    },
+  },
+})
 </script>
