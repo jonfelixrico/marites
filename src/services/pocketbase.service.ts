ffce06c144d5d190bee2ee02eba12bb3dcef2cc7
Felix <jonfelixrico@gmail.com>
2023-02-04 22:30:47 +0800

    Adjust the .env name

--- src/services/pocketbase.service.ts
+++ src/services/pocketbase.service.ts
@@ -1,6 +1,6 @@
 import Pocketbase from 'pocketbase'
 
-export const pocketbase = new Pocketbase(process.env.POCKETBASE_URI)
+export const pocketbase = new Pocketbase(process.env.PB_BASE_URL)
 
 export function usePocketbase() {
   return pocketbase
