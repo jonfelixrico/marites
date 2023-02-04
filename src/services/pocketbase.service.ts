4afa2181e488c2e4a135ee1f1d2c6f7bec73eb23
Felix <jonfelixrico@gmail.com>
2023-02-04 22:57:49 +0800

    Add debug logging

--- src/services/pocketbase.service.ts
+++ src/services/pocketbase.service.ts
@@ -1,6 +1,7 @@
 import Pocketbase from 'pocketbase'
 
 export const pocketbase = new Pocketbase(process.env.PB_BASE_URL)
+console.debug('Using %s as the Pocketbase URL')
 
 export function usePocketbase() {
   return pocketbase
