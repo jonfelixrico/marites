da48a3eb86e97f52ab90bd2588e33a8cb29d96ee
Felix <jonfelixrico@gmail.com>
2023-02-04 23:21:25 +0800

    Create ChatLayout

---
+++ src/layouts/ChatLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- TODO maybe use QLayout here? -->
+  <q-page class="row">
+    <router-view class="col" />
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
