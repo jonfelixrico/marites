78223ab430425913776fb64e4ae1b744952d0ec1
Felix <jonfelixrico@gmail.com>
2023-02-04 22:56:09 +0800

    Register LoginPage

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,7 +4,18 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      {
+        path: '',
+        name: 'index',
+        component: () => import('pages/IndexPage.vue'),
+      },
+      {
+        path: 'login',
+        name: 'login',
+        component: () => import('pages/LoginPage.vue'),
+      },
+    ],
   },
 
   // Always leave this as last one,
