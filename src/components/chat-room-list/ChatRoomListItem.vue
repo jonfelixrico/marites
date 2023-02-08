7a9970143ea476999f7630e8909aa906519c0781
Felix <jonfelixrico@gmail.com>
2023-02-09 00:21:46 +0800

    Init initial message load

--- src/components/chat-room-list/ChatRoomListItem.vue
+++ src/components/chat-room-list/ChatRoomListItem.vue
@@ -12,9 +12,12 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, PropType } from 'vue'
+import { computed, defineComponent, onBeforeMount, PropType } from 'vue'
 import { ChatRoom } from 'src/models/chat-room.interface'
 import { useChatRoomStore } from 'src/stores/chat-room.store'
+import { usePocketbase } from 'src/services/pocketbase.service'
+import { Message } from 'src/models/message.interface'
+import { toFilterDate } from 'src/utils/pocketbase.util'
 
 export default defineComponent({
   props: {
@@ -26,6 +29,29 @@ export default defineComponent({
 
   setup(props) {
     const store = useChatRoomStore()
+    const pb = usePocketbase()
+
+    onBeforeMount(async () => {
+      if (store.messagePreview[props.chatRoom.id]) {
+        return
+      }
+
+      const { items } = await pb.collection('messages').getList<Message>(1, 1, {
+        sort: '-created', // sorting by id to keep sorting consistent for same-timestamp messages
+        filter: `created <= "${toFilterDate(new Date())}" && chatRoomId = "${
+          props.chatRoom.id
+        }"`,
+        // multiple instances can load at a single instance, so we have to set this to false
+        // TODO set to false by default
+        $autoCancel: false,
+      })
+
+      if (!items.length) {
+        return
+      }
+
+      store.processMessage(items[0])
+    })
 
     return {
       message: computed(() => store.messagePreview[props.chatRoom.id]),
