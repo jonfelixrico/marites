245d8c55aa9f06b0a8839fe717ebb3a30b2c1c12
Felix <jonfelixrico@gmail.com>
2023-02-09 00:12:45 +0800

    Add test integration

--- src/components/chat-room-list/ChatRoomList.vue
+++ src/components/chat-room-list/ChatRoomList.vue
@@ -14,14 +14,16 @@ import { usePocketbase } from 'src/services/pocketbase.service'
 import { computed, defineComponent, onBeforeMount } from 'vue'
 import { useChatRoomStore } from 'src/stores/chat-room.store'
 import ChatRoomListItem from './ChatRoomListItem.vue'
+import { useMessageObservable } from 'src/services/message-observable.service'
 
 export default defineComponent({
   components: { ChatRoomListItem },
   setup() {
     const pb = usePocketbase()
     const store = useChatRoomStore()
+    const { observable } = useMessageObservable()
 
-    onBeforeMount(async () => {
+    async function loadChatRooms() {
       const items = await pb
         .collection('chatRooms')
         .getFullList<ChatRoom>(200, {
@@ -31,6 +33,15 @@ export default defineComponent({
       for (const item of items) {
         store.storeChatRoom(item)
       }
+    }
+
+    onBeforeMount(async () => {
+      loadChatRooms()
+      // TODO unsubscribe
+      observable.subscribe((message) => {
+        // TODO handle actions other than create
+        store.processMessage(message.record)
+      })
     })
 
     const chatRoomList = computed(() => store.chatRooms)
