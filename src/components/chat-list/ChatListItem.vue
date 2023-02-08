429d417154c5b28a5619608ca950eae0198dc8f8
Felix <jonfelixrico@gmail.com>
2023-02-08 23:46:20 +0800

    Create basic item component

---
+++ src/components/chat-list/ChatListItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-item>
+    <q-item-section>
+      <div>{{ chatRoom.name }}</div>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { ChatRoom } from 'src/models/chat-room.interface'
+
+export default defineComponent({
+  props: {
+    chatRoom: {
+      type: Object as PropType<ChatRoom>,
+      required: true,
+    },
+  },
+})
+</script>
