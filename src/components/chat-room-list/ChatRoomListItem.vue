b1c32092be5ef0797b11e4056c2358fb47782c94
Felix <jonfelixrico@gmail.com>
2023-02-09 00:08:44 +0800

    Integrate messagePreview

--- src/components/chat-room-list/ChatRoomListItem.vue
+++ src/components/chat-room-list/ChatRoomListItem.vue
@@ -6,13 +6,15 @@
   >
     <q-item-section>
       <div>{{ chatRoom.name }}</div>
+      <div>{{ message?.content }}</div>
     </q-item-section>
   </q-item>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { computed, defineComponent, PropType } from 'vue'
 import { ChatRoom } from 'src/models/chat-room.interface'
+import { useChatRoomStore } from 'src/stores/chat-room.store'
 
 export default defineComponent({
   props: {
@@ -20,6 +22,14 @@ export default defineComponent({
       type: Object as PropType<ChatRoom>,
       required: true,
     },
+  },
+
+  setup(props) {
+    const store = useChatRoomStore()
+
+    return {
+      message: computed(() => store.messagePreview[props.chatRoom.id]),
+    }
   },
 })
 </script>
