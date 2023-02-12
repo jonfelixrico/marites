cc339df6afa0a02248c9cac9a3767317f2563b82
Felix <jonfelixrico@gmail.com>
2023-02-12 21:42:40 +0800

    Remove unneeded stuff from MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,13 +1,5 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -17,7 +9,5 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-export default defineComponent({
-  name: 'MainLayout',
-})
+export default defineComponent({})
 </script>
