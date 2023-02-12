1e0d06a0b6801a6f3d9da1d026b669139a7fa495
Felix <jonfelixrico@gmail.com>
2023-02-12 22:04:16 +0800

    Create i18n for session-related stuff

---
+++ src/i18n/en-US/session.ts
@@ -0,0 +1,10 @@
+export default {
+  logoutPrompt: {
+    okLabel: 'Yes, log me out',
+    cancelLabel: 'No, keep me logged in',
+    title: 'Confirm log out',
+    message: 'Are you sure you want to log out of the app?',
+  },
+
+  logoutNotif: 'You have been logged out.',
+}
