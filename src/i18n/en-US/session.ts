59a3c8a1981532ae2ba3f9db2efcebca4e9699ac
Felix <jonfelixrico@gmail.com>
2023-02-12 22:08:38 +0800

    Adjust keys

--- src/i18n/en-US/session.ts
+++ src/i18n/en-US/session.ts
@@ -1,12 +1,12 @@
 export default {
-  logout: {
-    prompt: {
-      okLabel: 'Yes, log me out',
-      cancelLabel: 'No, keep me logged in',
-      title: 'Confirm log out',
-      message: 'Are you sure you want to log out of the app?',
-    },
+  logOut: 'Log out',
 
-    successNotif: 'You have been logged out.',
+  logOutPrompt: {
+    okLabel: 'Yes, log me out',
+    cancelLabel: 'No, keep me logged in',
+    title: 'Confirm log out',
+    message: 'Are you sure you want to log out of the app?',
   },
+
+  logOutSuccessNotif: 'You have been logged out',
 }
