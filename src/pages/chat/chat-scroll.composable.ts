f03b203e749227475c824141246256dc16e6d075
Felix <jonfelixrico@gmail.com>
2023-02-21 23:43:25 +0800

    Create composable for scrolling

---
+++ src/pages/chat/chat-scroll.composable.ts
@@ -0,0 +1,32 @@
+import { debounce, scroll } from 'quasar'
+import { ref } from 'vue'
+
+export function useChatScroll() {
+  const scrollEl = ref<HTMLElement | null>(null)
+  const scrollHeight = ref<number>(0)
+
+  const scrollListener = debounce((event: { target: HTMLElement }) => {
+    if (!event.target) {
+      return
+    }
+
+    if (!scrollEl.value) {
+      scrollEl.value = event.target
+    }
+
+    scrollHeight.value = event.target.scrollHeight
+  }, 250)
+
+  function compensateScroll() {
+    if (scrollEl.value && scrollHeight.value === 0) {
+      const scrollTarget = scroll.getScrollTarget(scrollEl.value)
+      scroll.setVerticalScrollPosition(scrollTarget, 0)
+      console.debug('Scroll compensated.')
+    }
+  }
+
+  return {
+    scrollListener,
+    compensateScroll,
+  }
+}
