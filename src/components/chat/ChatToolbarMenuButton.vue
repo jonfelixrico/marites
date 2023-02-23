7d0ab858a28ee2a037339a25934a61bd378cdcff
Felix <jonfelixrico@gmail.com>
2023-02-24 00:30:47 +0800

    Create stub

---
+++ src/components/chat/ChatToolbarMenuButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-btn icon="menu">
+    <q-menu>
+      <q-list>
+        <q-item></q-item>
+      </q-list>
+    </q-menu>
+  </q-btn>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
