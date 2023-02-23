6355360f3906b5d2b57a45933ccb0d29ebda8d7d
Felix <jonfelixrico@gmail.com>
2023-02-24 00:33:45 +0800

    Create submenu

--- src/i18n/en-US/chat.ts
+++ src/i18n/en-US/chat.ts
@@ -1,23 +1,5 @@
+import chatToolbar from './chat-toolbar'
+
 export default {
-  addUser: 'Add user to chat',
-  dialogs: {
-    addUser: {
-      title: 'Add user to chat',
-      message: 'Enter the username of the user to add to the chat',
-    },
-
-    addUserError: {
-      title: 'Cannot add user',
-      message: {
-        notFound: 'Cannot find user with the username {username}',
-        alreadyAdded: 'User {username} is already in the chat!',
-        generic:
-          'Something went wrong while trying to add user {username} into the chat',
-      },
-    },
-  },
-
-  notifs: {
-    addUserSucess: 'Successfully added user {username} into the chat',
-  },
+  toolbar: chatToolbar,
 }
