9eda3b266c3372d840bbbc3578ffe040c976b1bb
Felix <jonfelixrico@gmail.com>
2023-02-26 03:32:14 +0800

    Remove deprecated code

--- src/models/pb-collection.enum.ts
+++ src/models/pb-collection.enum.ts
@@ -1,10 +1,6 @@
 export enum PBCollection {
   CHAT = 'chat',
   CHAT_MESSAGE = 'chatMessage',
-  /**
-   * @deprecated
-   */
-  CHAT_MEMBER = 'chatMember',
   CHAT_USER_MEMBERSHIP = 'chatUserMembership',
 
   USER = 'users',
