9152dd20eee3b6b7e973260ef36c5c5b40588822
Felix <jonfelixrico@gmail.com>
2023-02-25 23:28:17 +0800

    Apply minor changes

--- src/composables/chat-message-api.composable.ts
+++ src/composables/chat-message-api.composable.ts
@@ -21,7 +21,10 @@ export function useChatMessageApi() {
   const pb = usePocketbase()
   const sessionApi = useSessionApi()
 
-  async function createMessage({ content, chatId }: SendMessageBody) {
+  async function createMessage({
+    content,
+    chatId,
+  }: SendMessageBody): Promise<APIChatMessage> {
     pb.collection(PbCollection.CHAT_MESSAGE).create<APIChatMessage>({
       content,
       sender: sessionApi.getSessionUser(),
@@ -29,7 +32,9 @@ export function useChatMessageApi() {
     })
   }
 
-  async function getLastMessage(chatId: string) {
+  async function getLastMessage(
+    chatId: string
+  ): Promise<APIChatMessage | null> {
     const { items } = await pb
       .collection(PbCollection.CHAT_MESSAGE)
       // can't use getFirstListItem here since it doesn't allow us to specify the ordering
