bfdb7a677980fb4f824cb5fca9f0d239d6ee77c5
Felix <jonfelixrico@gmail.com>
2023-02-26 00:09:31 +0800

    Adjust the PB collection enum

--- src/models/pb-collection.enum.ts
+++ src/models/pb-collection.enum.ts
@@ -1,6 +1,11 @@
 export enum PbCollection {
   CHAT = 'chat',
   CHAT_MESSAGE = 'chatMessage',
+  /**
+   * @deprecated
+   */
   CHAT_MEMBER = 'chatMember',
+  CHAT_USER_MEMBERSHIP = 'chatUserMembership',
+
   USER = 'users',
 }
