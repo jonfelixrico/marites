575794a1b6e39d88787d07c2abedb964caf1b2ae
Felix <jonfelixrico@gmail.com>
2023-02-25 23:27:39 +0800

    Create retrieval tool for last message

--- src/composables/chat-message-api.composable.ts
+++ src/composables/chat-message-api.composable.ts
@@ -1,5 +1,6 @@
 import { PbCollection } from 'src/models/pb-collection.enum'
 import { usePocketbase } from 'src/services/pocketbase.service'
+import { toFilterDate } from 'src/utils/pocketbase.util'
 import { useSessionApi } from './session-api.composable'
 
 interface SendMessageBody {
@@ -7,27 +8,43 @@ interface SendMessageBody {
   content: string
 }
 
-interface APISendMessageResult {
+interface APIChatMessage {
   content: string
   chat: string
   user: string
   created: Date
   updated: Date
+  id: string
 }
 
 export function useChatMessageApi() {
   const pb = usePocketbase()
   const sessionApi = useSessionApi()
 
-  async function sendMessage({ content, chatId }: SendMessageBody) {
-    pb.collection(PbCollection.CHAT_MESSAGE).create<APISendMessageResult>({
+  async function createMessage({ content, chatId }: SendMessageBody) {
+    pb.collection(PbCollection.CHAT_MESSAGE).create<APIChatMessage>({
       content,
       sender: sessionApi.getSessionUser(),
       chat: chatId,
     })
   }
 
+  async function getLastMessage(chatId: string) {
+    const { items } = await pb
+      .collection(PbCollection.CHAT_MESSAGE)
+      // can't use getFirstListItem here since it doesn't allow us to specify the ordering
+      .getList<APIChatMessage>(1, 1, {
+        sort: '-created', // sorting by id to keep sorting consistent for same-timestamp messages
+        filter: `created <= "${toFilterDate(
+          new Date()
+        )}" && chat = "${chatId}"`,
+      })
+
+    return items[0] ?? null
+  }
+
   return {
-    sendMessage,
+    createMessage,
+    getLastMessage,
   }
 }
