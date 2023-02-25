1b71fb6b5deafbd5e1a9214006b69cc2b88a9d00
Felix <jonfelixrico@gmail.com>
2023-02-26 01:47:21 +0800

    Create the subscription manager

--- src/services/subscription-manager.service.ts
+++ src/services/subscription-manager.service.ts
@@ -1,14 +1,18 @@
 import { RecordSubscription, UnsubscribeFunc } from 'pocketbase'
-import { Subject } from 'rxjs'
+import { filter, map, Observable, Subject } from 'rxjs'
+import { PbCollection } from 'src/models/pb-collection.enum'
+import { usePocketbase } from './pocketbase.service'
 
 interface GenericRecordSubscription extends RecordSubscription<unknown> {
   collection: string
 }
 
-const subject = new Subject<RecordSubscription>()
+const subject = new Subject<GenericRecordSubscription>()
 const unsubscribers: Record<string, UnsubscribeFunc> = {}
 
 export function useSubscriptionManager() {
+  const pb = usePocketbase()
+
   function unsubscribe() {
     console.debug('Starting the unsubscription process...')
 
@@ -21,7 +25,39 @@ export function useSubscriptionManager() {
     console.log('Killed all subscriptions.')
   }
 
+  async function createSubscription(collection: PbCollection) {
+    const unsubscriber = await pb
+      .collection(collection)
+      .subscribe('*', (event) => {
+        subject.next({
+          ...event,
+          collection,
+        })
+      })
+
+    unsubscribers[collection] = unsubscriber
+  }
+
+  async function getSubscription<T>(
+    collection: PbCollection
+  ): Promise<Observable<RecordSubscription<T>>> {
+    if (!unsubscribers[collection]) {
+      await createSubscription(collection)
+    }
+
+    return subject.pipe(
+      filter((event) => event.collection === collection),
+      map(({ action, record }) => {
+        return {
+          action,
+          record: record as T, // we'll just trust the caller that they gave the proper type
+        }
+      })
+    )
+  }
+
   return {
     unsubscribe,
+    getSubscription,
   }
 }
