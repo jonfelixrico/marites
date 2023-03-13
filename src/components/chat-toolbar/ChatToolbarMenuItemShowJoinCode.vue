7f11ccfd03e3457141015e21285358cec539c60b
Felix <jonfelixrico@gmail.com>
2023-03-14 02:41:27 +0800

    Integrate the dialog

--- src/components/chat-toolbar/ChatToolbarMenuItemShowJoinCode.vue
+++ src/components/chat-toolbar/ChatToolbarMenuItemShowJoinCode.vue
@@ -1,46 +1,17 @@
 <template>
-  <q-item clickable @click="showDialog = true" v-bind="$attrs">
+  <q-item clickable @click="showDialog">
     <q-item-section>
       {{ $t('chat.toolbar.showJoinCode') }}
     </q-item-section>
   </q-item>
-
-  <q-dialog v-model="showDialog">
-    <q-card>
-      <q-card-section class="text-h6 q-pb-none">
-        {{ $t('chat.toolbar.dialog.showJoinCode.title') }}
-      </q-card-section>
-
-      <q-card-section>
-        <i18n-t keypath="chat.toolbar.dialog.showJoinCode.message" tag="div">
-          <template #joinCode>
-            <q-btn dense no-caps unelevated @click="copyCode" v-close-popup>
-              <div class="q-gutter-x-xs">
-                <span>{{ chat.joinCode }}</span>
-                <q-icon
-                  class="text-weight-bold"
-                  name="content_copy"
-                  size="xs"
-                />
-              </div>
-            </q-btn>
-          </template>
-        </i18n-t>
-      </q-card-section>
-
-      <q-card-actions align="right">
-        <q-btn color="primary" no-caps unelevated>{{ $t('general.ok') }}</q-btn>
-      </q-card-actions>
-    </q-card>
-  </q-dialog>
 </template>
 
 <script lang="ts">
+import { useQuasar } from 'quasar'
 import { useChatIdFromRoute } from 'src/composables/route-chat-id.composable'
 import { useChatStore } from 'src/stores/chat.store'
-import { defineComponent, computed, ref } from 'vue'
-import { copyToClipboard, useQuasar } from 'quasar'
-import { useI18n } from 'vue-i18n'
+import { defineComponent, computed } from 'vue'
+import ChatToolbarMenuItemShowJoinCodeDialog from './ChatToolbarMenuItemShowJoinCodeDialog.vue'
 
 export default defineComponent({
   setup() {
@@ -48,20 +19,20 @@ export default defineComponent({
     const store = useChatStore()
     const chat = computed(() => store.chats[chatId.value])
 
-    const showDialog = ref(false)
+    const { dialog } = useQuasar()
 
-    const { notify } = useQuasar()
-    const { t } = useI18n()
-
-    async function copyCode() {
-      await copyToClipboard(chat.value?.joinCode)
-      notify(t('chat.toolbar.notif.joinCodeCopied'))
+    function showDialog() {
+      dialog({
+        component: ChatToolbarMenuItemShowJoinCodeDialog,
+        componentProps: {
+          joinCode: chat.value.joinCode,
+        },
+      })
     }
 
     return {
       chat,
       showDialog,
-      copyCode,
     }
   },
 })
