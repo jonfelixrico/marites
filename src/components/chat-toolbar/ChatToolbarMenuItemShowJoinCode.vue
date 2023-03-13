2b61aa39d0d792d996d12b860428ed44df7d56c3
Felix <jonfelixrico@gmail.com>
2023-03-14 02:15:19 +0800

    Create menu item

---
+++ src/components/chat-toolbar/ChatToolbarMenuItemShowJoinCode.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-item clickable @click="showDialog">
+    <q-item-section>
+      {{ $t('chat.toolbar.showJoinCode') }}
+    </q-item-section>
+  </q-item>
+</template>
+
+<script lang="ts">
+import { useChatIdFromRoute } from 'src/composables/route-chat-id.composable'
+import { useChatStore } from 'src/stores/chat.store'
+import { defineComponent, computed } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const chatId = useChatIdFromRoute()
+    const store = useChatStore()
+
+    return {
+      chat: computed(() => store.chats[chatId.value]),
+    }
+  },
+
+  methods: {
+    showDialog() {
+      // TODO implement stuff
+    },
+  },
+})
+</script>
