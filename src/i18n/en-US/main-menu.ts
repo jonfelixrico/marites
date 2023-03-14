57d22aef3adb4571b7e2baa2238cca555b41ba62
Felix <jonfelixrico@gmail.com>
2023-03-14 22:09:26 +0800

    Create i18n for join chat

--- src/i18n/en-US/main-menu.ts
+++ src/i18n/en-US/main-menu.ts
@@ -1,4 +1,16 @@
 export default {
   joinChat: 'Join chat',
   createChat: 'Create chat',
+
+  dialog: {
+    joinChat: {
+      title: 'Join chat',
+      message: 'Please enter the join code of the chat that you want to enter',
+
+      error: {
+        alreadyJoined: 'You already are a member of chat with code {joinCode}',
+        notFound: 'No chat rooms match join code {joinCode}',
+      },
+    },
+  },
 }
