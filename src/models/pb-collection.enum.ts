55cad137715fc4f1e4f83f810aece226eec818e8
Felix <jonfelixrico@gmail.com>
2023-03-14 23:05:30 +0800

    Include joinCode

--- src/models/pb-collection.enum.ts
+++ src/models/pb-collection.enum.ts
@@ -2,6 +2,7 @@ export enum PBCollection {
   CHAT = 'chat',
   CHAT_MESSAGE = 'chatMessage',
   CHAT_USER_MEMBERSHIP = 'chatUserMembership',
+  CHAT_JOIN_CODE = 'chatJoinCode',
 
   USER = 'users',
 }
