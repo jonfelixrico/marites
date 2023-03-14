85b5b8cae0fff9d6a8590866fdbded9ed58d9dea
Felix <jonfelixrico@gmail.com>
2023-03-14 22:12:36 +0800

    Create i18n dialog

---
+++ src/components/main-menu/MainMenuItemJoinChatDialog.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-dialog ref="dialogRef" @hide="onDialogHide">
+    <q-card>
+      <q-card-section class="text-h6 q-pb-none">
+        {{ $t('mainMenu.joinChat.title') }}
+      </q-card-section>
+
+      <q-card-actions align="right">
+        <q-btn color="primary" no-caps unelevated>{{
+          $t('mainMenu.dialog.joinChat.join')
+        }}</q-btn>
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useDialogPluginComponent } from 'quasar'
+
+export default defineComponent({
+  emits: [...useDialogPluginComponent.emits],
+
+  setup() {
+    return {
+      ...useDialogPluginComponent(),
+    }
+  },
+})
+</script>
