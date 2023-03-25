92c497f73c57035fb681c328a26273a4a77aec14
Felix <jonfelixrico@gmail.com>
2023-03-25 18:25:10 +0800

    Add copyNotif prop

--- src/components/ClickToCopyBtn.vue
+++ src/components/ClickToCopyBtn.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script lang="ts">
-import { copyToClipboard } from 'quasar'
+import { copyToClipboard, useQuasar } from 'quasar'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
@@ -22,14 +22,25 @@ export default defineComponent({
       type: String,
       default: 'primary',
     },
+
+    copyNotif: String,
   },
 
   emits: ['copy'],
 
   setup(props, { emit }) {
+    const { notify } = useQuasar()
+
     async function copyContent() {
-      await copyToClipboard(props.content)
-      emit('copy', props.content)
+      const { content, copyNotif } = props
+
+      await copyToClipboard(content)
+      console.debug('Copied content %s to clipboard', content)
+      emit('copy', content)
+
+      if (copyNotif) {
+        notify(copyNotif)
+      }
     }
 
     return {
