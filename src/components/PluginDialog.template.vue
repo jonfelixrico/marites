b4a96417661597c4b36f8455c1ef10e2d48a7ad1
Felix <jonfelixrico@gmail.com>
2023-03-25 13:00:34 +0800

    Create template component

---
+++ src/components/PluginDialog.template.vue
@@ -0,0 +1,22 @@
+<!-- This snippet below is a template for creating dialog plugin components -->
+
+<template>
+  <q-dialog ref="dialogRef" @hide="onDialogHide"></q-dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useDialogPluginComponent } from 'quasar'
+
+export default defineComponent({
+  emits: [...useDialogPluginComponent.emits],
+
+  setup() {
+    const pluginComp = useDialogPluginComponent()
+
+    return {
+      ...pluginComp,
+    }
+  },
+})
+</script>
