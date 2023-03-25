32881643efdd401af6b3cf925991f82f9b55594e
Felix <jonfelixrico@gmail.com>
2023-03-25 18:55:46 +0800

    Add functionality

--- src/components/main-menu/MainMenuItemShowUserCodeDialog.vue
+++ src/components/main-menu/MainMenuItemShowUserCodeDialog.vue
@@ -18,7 +18,7 @@
       </q-card-section>
 
       <q-card-actions align="between">
-        <q-btn no-caps flat color="negative">
+        <q-btn no-caps flat color="negative" @click="handleResetClick">
           {{ t('mainMenu.dialog.userCodeShow.reset') }}
         </q-btn>
         <q-btn no-caps unelevated color="primary" v-close-popup autofocus>
@@ -31,9 +31,64 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { useDialogPluginComponent } from 'quasar'
+import { useDialogPluginComponent, useQuasar } from 'quasar'
 import { useI18n } from 'vue-i18n'
 import ClickToCopyBtn from 'components/ClickToCopyBtn.vue'
+import { useDialogHelper } from 'src/composables/quasar-helper.composable'
+import { useUserCodeAPI } from 'src/composables/user-api.composable'
+import { useSessionApi } from 'src/composables/session-api.composable'
+
+function useResetUserCode() {
+  const { t } = useI18n()
+  const { showBasicDialog, showBasicErrorDialog } = useDialogHelper()
+  const { resetUserCode } = useUserCodeAPI()
+  const { getSessionUser } = useSessionApi()
+  const { loading } = useQuasar()
+
+  function showPrompt() {
+    return new Promise<boolean>((resolve) => {
+      showBasicDialog({
+        title: t('mainMenu.dialog.userCodeResetConfirm.title'),
+        message: t('mainMenu.dialog.userCodeResetConfirm.message'),
+        okLabel: t('mainMenu.dialog.userCodeResetConfirm.ok'),
+        cancelLabel: t('mainMenu.dialog.userCodeResetConfirm.cancel'),
+      })
+        .onOk(() => resolve(true))
+        .onCancel(() => resolve(false))
+    })
+  }
+
+  function showSuccessDialog(userCode: string) {
+    showBasicDialog({
+      title: t('mainMenu.dialog.userCodeResetSuccess.title'),
+      // TODO use custom dialog for the copy button for userCode
+      message: t('mainMenu.dialog.userCodeResetSuccess.message', {
+        userCode,
+      }),
+    })
+  }
+
+  async function resetCode() {
+    const didUserConfirm = await showPrompt()
+    if (!didUserConfirm) {
+      return
+    }
+
+    try {
+      loading.show()
+      const newUserCode = await resetUserCode(getSessionUser().id)
+      showSuccessDialog(newUserCode)
+    } catch (e) {
+      showBasicErrorDialog()
+    } finally {
+      loading.hide()
+    }
+  }
+
+  return {
+    resetCode,
+  }
+}
 
 export default defineComponent({
   components: { ClickToCopyBtn },
@@ -48,10 +103,17 @@ export default defineComponent({
   setup() {
     const pluginComp = useDialogPluginComponent()
     const { t } = useI18n()
+    const { resetCode } = useResetUserCode()
+
+    function handleResetClick() {
+      pluginComp.onDialogHide()
+      resetCode()
+    }
 
     return {
       ...pluginComp,
       t,
+      handleResetClick,
     }
   },
 })
