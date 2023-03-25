5fca5b298f1579f08a70f82affcaedb76687c9e8
Felix <jonfelixrico@gmail.com>
2023-03-25 12:59:37 +0800

    Create stub code

---
+++ src/components/main-menu/MainMenuItemShowUserCode.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-item clickable>
+    <q-item-section>
+      {{ $t('mainMenu.joinChat') }}
+    </q-item-section>
+  </q-item>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
---
+++ src/components/main-menu/MainMenuItemShowUserCodeDialog.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-dialog ref="dialogRef" @hide="onDialogHide"></q-dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useDialogPluginComponent } from 'quasar'
+
+export default defineComponent({
+  emits: [...useDialogPluginComponent.emits],
+
+  setup() {
+    const pluginComp = useDialogPluginComponent()
+
+    return {
+      ...pluginComp,
+    }
+  },
+})
+</script>
